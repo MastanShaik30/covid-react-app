0bd0dc5cd8f13e921fcbfd05beb6138578aaee41
Mastan Shaik <mastan30@gmail.com>
2020-04-04 11:24:56 -0400

    added new end point using rapid api

--- src/components/CovidData/GetCovidData.js
+++ src/components/CovidData/GetCovidData.js
@@ -17,12 +17,19 @@ class GetCovidData extends Component {
         // Using europe data
         // this return covid data 
         // fetch("https://opendata.ecdc.europa.eu/covid19/casedistribution/json/",{ 
-            fetch("https://jsonplaceholder.typicode.com/photos",{  
-            method: 'GET',
-            headers:{
-                'Access-Control-Allow-Origin': '*'
+        //     fetch("https://jsonplaceholder.typicode.com/photos",{  
+        //     method: 'GET',
+        //     headers:{
+        //         'Access-Control-Allow-Origin': '*'
+        //     }
+        //   })
+        fetch("https://covid-193.p.rapidapi.com/statistics", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "covid-193.p.rapidapi.com",
+                "x-rapidapi-key": "088854f95bmsh82d19f9ac952d36p1da956jsn2401cfa29608"
             }
-          })
+        })
         .then( response => response.json())
         .then(
             // handle the result
@@ -45,14 +52,28 @@ class GetCovidData extends Component {
 
     render() {
         const {error, isLoaded, records} = this.state;
-        console.log(JSON.stringify(records));
+
+        console.log(JSON.stringify(final));
         if(error){
             return <div>Failed loading</div>
         }else if (!isLoaded) {
             return <div>Loading ...</div>
         }else{
+            var responses = [];
+            responses = records.response;
+            var final = [];
+            responses.map((response, index) => {
+                final.push({
+                    country: response.country,
+                    active_cases: response.cases.active,
+                    total_cases: response.cases.total,
+                    total_deaths: response.deaths.total,
+                    update_time: response.time
+                }
+                )
+            });
             return(
-                    <Tablejson data={records}/>
+                    <Tablejson data={final}/>
                 // <MDBDataTable striped bordered hover data={records} />
             );
         }
