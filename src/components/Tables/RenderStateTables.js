75ae3c0d0bfaa418ba0dab8ca2470c15831a9f8e
Mastan Shaik <mastan30@gmail.com>
2020-04-06 22:37:41 -0400

    added districts data table

--- src/components/Tables/RenderStateTables.js
+++ src/components/Tables/RenderStateTables.js
@@ -1,17 +1,22 @@
 import React from "react";
 import Accordion from "../Accordion";
-import MaterialTable from "./MaterialTable";
+import DTable from "./Table";
 import { renderToString } from 'react-dom/server';
 
 export default props => {
     if(props.districts != undefined){ 
-    var districtTable = renderToString(<MaterialTable data={props.districts}/>);
-    console.log(districtTable);
     console.log('All Districts Data  = '+ JSON.stringify(props.districts));
+    var table = renderToString(<DTable data={props.districts}/>);
+    console.log(table)
+    var list="<h4>Confirmed</h4></br><ul>";
+    props.districts.map((district,index) =>{
+      list += "<li><span>"+district.district+"</span> : <span>"+district.confirmed+"</span></li>";
+    });
+    list+="</ul>";
     return(
         <Accordion
             title={props.state}
-            content={districtTable}
+            content={table}
           />
         
       );
